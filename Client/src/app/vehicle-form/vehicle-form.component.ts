57595840d2bbc04c6b0d42f37dbd38aab17a4181
KetelsenCode <pardonmychessmate@gmail.com>
2019-03-18 18:09:34 +0100

    Mostly comments

--- Client/src/app/vehicle-form/vehicle-form.component.ts
+++ Client/src/app/vehicle-form/vehicle-form.component.ts
@@ -23,41 +23,47 @@ export class VehicleFormComponent implements OnInit {
   }
 
   ngOnInit() {
+
+    //Get all brands with include on models @ EF 
     this.BrandService.getBrands().
                     subscribe(brands => {
                       this.brands = brands;
                       console.log(brands);
                      })
 
+    //Get all features
     this.FeatureService.getFeatures().
                      subscribe(features => {
                        this.features = features;
                        console.log(this.features);
                      })
   }
+
+  //When a new brand gets picked from the first menu
   onBrandChange(){
     var selectedBrand = this.brands.find(b => b.id == this.vehicle.brandId);
     this.models = selectedBrand.models;
-    
-    
   }
+
+  //When a new model gets picked from the model menu
   onModelChange(){
     
     var selectedModel = this.models.find(m => m.id == this.vehicle.modelId);
     console.log(selectedModel);
   }
 
+  //Gets called everytime there's a change in the checkboxes in features
   onFeatureChange(featureId, $event)
-  {
-    if ($event.target.checked)
+  { 
+    if ($event.target.checked) //If it gets toggled
     { //We're pushing the Id only cause features and vehicles has a many-2-many relationship and there's a table inbetween holding both keys
       this.vehicle.features.push(featureId);
     }
-    else {
-      console.log(this.vehicle.features);
-      var index = this.vehicle.features.indexOf(featureId);
-      this.vehicle.features.splice(index,1);
-      console.log(this.vehicle.features);
+    else { //If it gets untoggled
+     
+      var index = this.vehicle.features.indexOf(featureId); //gets the index of the clicked button
+      this.vehicle.features.splice(index,1); //removes one from where its index is. 
+     ;
     }
   }
 
